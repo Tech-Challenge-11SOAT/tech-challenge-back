9e75591b4978ce16cbbcba20c7e70a2f6a2b668a
Pedro <pedro.argentatii@gmail.com>
2025-04-22 19:41:39 -0300

    initial commit

---
+++ src/main/java/br/com/postech/techchallange/TechchallangeApplication.java
@@ -0,0 +1,13 @@
+package br.com.postech.techchallange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechchallangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechchallangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/postech/techchallange/TechchallangeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.postech.techchallange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechchallangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
