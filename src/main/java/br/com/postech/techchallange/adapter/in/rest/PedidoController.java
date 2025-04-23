ea100ca934ef83999cfcd87a3581ba6313e27d3c
Pedro <pedro.argentatii@gmail.com>
2025-04-22 21:11:16 -0300

    feature: swagger adicionado

--- src/main/java/br/com/postech/techchallange/adapter/in/rest/PedidoController.java
+++ src/main/java/br/com/postech/techchallange/adapter/in/rest/PedidoController.java
@@ -2,12 +2,18 @@ package br.com.postech.techchallange.adapter.in.rest;
 
 import br.com.postech.techchallange.domain.model.Pedido;
 import br.com.postech.techchallange.domain.port.in.GerenciarPedidoUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.Parameter;
+import jakarta.validation.Valid;
+
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/pedidos")
+@Tag(name = "Pedidos", description = "Operações relacionadas aos pedidos")
 public class PedidoController {
 
 	private final GerenciarPedidoUseCase pedidoUseCase;
@@ -16,16 +22,25 @@ public class PedidoController {
 		this.pedidoUseCase = pedidoUseCase;
 	}
 
+	@Operation(summary = "Criar um novo pedido")
 	@PostMapping
-	public Pedido criar(@RequestBody Pedido pedido) {
+	public Pedido criar(
+			@Valid
+			@RequestBody
+			@io.swagger.v3.oas.annotations.parameters.RequestBody(
+					description = "Dados do pedido a ser criado",
+					required = true)
+			Pedido pedido) {
 		return pedidoUseCase.criarPedido(pedido);
 	}
 
+	@Operation(summary = "Buscar um pedido pelo ID")
 	@GetMapping("/{id}")
-	public Pedido buscar(@PathVariable Long id) {
+	public Pedido buscar(@Parameter(description = "ID do pedido", example = "1") @PathVariable Long id) {
 		return pedidoUseCase.buscarPedido(id);
 	}
 
+	@Operation(summary = "Listar todos os pedidos")
 	@GetMapping
 	public List<Pedido> listar() {
 		return pedidoUseCase.listarPedidos();
