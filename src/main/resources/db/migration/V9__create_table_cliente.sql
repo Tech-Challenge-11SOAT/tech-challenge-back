746961ffb341d9585a1456eb2ace3256d730bf91
Endrew <endrewslvp@gmail.com>
2025-04-29 23:11:37 -0300

    feat: adicionando funcionalidade do Cliente

---
+++ src/main/resources/db/migration/V9__create_table_cliente.sql
@@ -0,0 +1,6 @@
+CREATE TABLE cliente (
+    id_cliente INT AUTO_INCREMENT PRIMARY KEY,
+    nome_cliente VARCHAR(100) NOT NULL,
+    email_cliente VARCHAR(100) NOT NULL,
+    cpf_cliente VARCHAR(11) NOT NULL
+);
