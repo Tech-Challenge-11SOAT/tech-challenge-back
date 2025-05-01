bc391fae08ac2c4221e8e451b96e321a97a904a2
Pedro <pedro.argentatii@gmail.com>
2025-04-30 21:39:33 -0300

    fix: ajustes ClienteEntity

--- src/main/java/br/com/postech/techchallange/adapter/out/persistence/entity/ClienteEntity.java
+++ src/main/java/br/com/postech/techchallange/adapter/out/persistence/entity/ClienteEntity.java
@@ -1,6 +1,9 @@
 package br.com.postech.techchallange.adapter.out.persistence.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,13 +17,22 @@ import lombok.Setter;
 @NoArgsConstructor
 public class ClienteEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long idCliente;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_cliente")
+	private Long idCliente;
 
-    private String nomeCliente;
+	@NotBlank(message = "O nome é obrigatório.")
+	@Column(name = "nome_cliente", length = 100, nullable = false)
+	private String nomeCliente;
 
-    private String emailCliente;
+	@NotBlank(message = "O e-mail é obrigatório.")
+	@Email(message = "Formato de e-mail inválido.")
+	@Column(name = "email_cliente", length = 100, nullable = false)
+	private String emailCliente;
 
-    private String cpfCliente;
+	@NotBlank(message = "O CPF é obrigatório.")
+	@Pattern(regexp = "\\d{11}", message = "O CPF deve conter exatamente 11 dígitos numéricos.")
+	@Column(name = "cpf_cliente", length = 11, nullable = false)
+	private String cpfCliente;
 }
