6151a493cc8812f11db875510008a4a0f53ed717
Pedro <pedro.argentatii@gmail.com>
2025-05-05 21:35:55 -0300

    fix: correção de bug status pagamento

--- src/main/java/br/com/postech/techchallange/adapter/out/persistence/entity/StatusPagamentoEntity.java
+++ src/main/java/br/com/postech/techchallange/adapter/out/persistence/entity/StatusPagamentoEntity.java
@@ -6,13 +6,17 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "status_pagamento")
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class StatusPagamentoEntity {
 
 	@Id
