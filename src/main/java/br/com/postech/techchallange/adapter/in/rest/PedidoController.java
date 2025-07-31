a6254744bc9c28d512a07271c8bf717f305ffc4c
Pedro Argentati <pedro.argentatii@gmail.com>
2025-07-30 21:06:07 -0300

    refactor: streamline pedido filtering and sorting logic in listarPedidosCozinha method

--- src/main/java/br/com/postech/techchallange/adapter/in/rest/PedidoController.java
+++ src/main/java/br/com/postech/techchallange/adapter/in/rest/PedidoController.java
@@ -1,18 +1,5 @@
 package br.com.postech.techchallange.adapter.in.rest;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
 import br.com.postech.techchallange.adapter.in.rest.request.AtualizarPedidoRequest;
 import br.com.postech.techchallange.adapter.in.rest.request.FakeCheckoutRequest;
 import br.com.postech.techchallange.adapter.in.rest.response.PedidoCompletoResponse;
@@ -26,6 +13,11 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/open/pedidos")
@@ -83,7 +75,7 @@ public class PedidoController {
     @Operation(summary = "Listar pedidos para cozinha (ordenados e filtrados)")
     public List<PedidoCompletoResponse> listarPedidosCozinha() {
         List<PedidoCompletoResponse> todos = this.pedidoUseCase.listarPedidos();
-        List<PedidoCompletoResponse> filtrados = todos.stream()
+        return todos.stream()
                 .filter(p -> p.status() != null && !"FINALIZADO".equalsIgnoreCase(p.status().nome()))
                 .sorted((p1, p2) -> {
                     int statusOrder1 = statusOrder(p1.status().nome());
@@ -94,7 +86,6 @@ public class PedidoController {
                     return p1.dataPedido().compareTo(p2.dataPedido());
                 })
                 .toList();
-        return filtrados;
     }
 
     private int statusOrder(String status) {
